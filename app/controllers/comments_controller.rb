581706a4178c730ebf04bca0df2ea6f543d82759
Christian Rojas <christiandiegor@gmail.com>
2016-02-02 16:52:17 -0300

    se pueden subir imagenes y comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
 	before_action :set_comment, only: [:show, :edit, :update, :destroy]
-	before_action :set_artice
+	before_action :set_article
 	before_action :authenticate_user!
 
 	def index
@@ -59,7 +59,7 @@ class CommentsController < ApplicationController
 			@article = Comment.find(:params[:id])
 		end
 
-		def set_artice
+		def set_article
 			@article = Article.find(params[:article_id])
 		end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 	 		:recoverable, :rememberable, :trackable, :validatable
 
 	has_many :articles
+	has_many :comments
 
 	include PermissionsConcern
 
