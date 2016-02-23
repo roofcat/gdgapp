f74d20275c2398d1e3ff669249cccefdab0c35d5
Christian Rojas <christiandiegor@gmail.com>
2016-02-23 11:20:27 -0300

    mejoras en el template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  Paperclip.options[:command_path] = "C:\Program Files\ImageMagick"
+  #Paperclip.options[:command_path] = "C:\Program Files\ImageMagick"
 end
