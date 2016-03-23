5da4c2a92798f35551d8f6a3ced5f2ff7f6712a0
Christian Rojas <christiandiegor@gmail.com>
2016-03-23 15:33:34 -0300

    instalacion de active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+	# devise_for :admin_users, ActiveAdmin::Devise.config
+	# ActiveAdmin.routes(self)
+	
 	resources :about_gdgs
 
 	resources :categories
